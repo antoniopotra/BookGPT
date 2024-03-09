af48751e113d6119794fa9d8cee2f147f46ecbdb
antoniopotra <antonio.potra@gmail.com>
2024-03-09 17:45:32 +0200

    initial commit

---
+++ src/main/java/software/design/bookgpt/BookGptApplication.java
@@ -0,0 +1,13 @@
+package software.design.bookgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/software/design/bookgpt/BookGptApplicationTests.java
@@ -0,0 +1,13 @@
+package software.design.bookgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
